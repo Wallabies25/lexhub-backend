e6f57b292b04390b3e128e64cfaeec3b6c41fb9d
SinethNR <nimhanrds.22@uom.lk>
2026-04-01 20:56:59 +0530

    Robust DB connection cleaning

--- app/database.py
+++ app/database.py
@@ -2,11 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER     = os.getenv("DB_USER",     "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
-DB_HOST     = os.getenv("DB_HOST",     "localhost")
-DB_PORT     = os.getenv("DB_PORT",     "3306")
-DB_NAME     = os.getenv("DB_NAME",     "lexhub")
+DB_USER     = os.getenv("DB_USER",     "root").strip()
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root").strip()
+DB_HOST     = os.getenv("DB_HOST",     "localhost").strip()
+DB_PORT     = os.getenv("DB_PORT",     "3306").strip()
+DB_NAME     = os.getenv("DB_NAME",     "lexhub").strip()
+
 
 IS_AIVEN = "aivencloud.com" in DB_HOST
 
